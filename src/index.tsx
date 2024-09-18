205adb446efd80b57657d5467f2a230be1a028e1
Michael Owens <107378412+mowens24@users.noreply.github.com>
2024-09-18 18:04:38 -0400

    Added base.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import awsExports from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
+import '../../styles/base.scss';
 // Amplify(awsExports);//
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+createRoot(document.getElementById('app')!).render(<App />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/interfaces.ts
@@ -0,0 +1,6 @@
+export interface StoredWidgetPlacement {
+    id: string;
+    columnOffset?: Record<number, number>;
+    rowSpan?: number;
+    columnSpan?: number;
+  }
